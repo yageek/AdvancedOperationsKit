63e04144b47c343d2f154e0df192448cdc0a264e
Yannick Heinrich <yannick.heinrich@gmail.com>
2019-12-22 22:19:42 +0100

    Initial Commit

---
+++ AdvancedOperationsKit/AdvancedOperationsKit.h
@@ -0,0 +1,19 @@
+//
+//  AdvancedOperationsKit.h
+//  AdvancedOperationsKit
+//
+//  Created by Yannick Heinrich on 22.12.19.
+//  Copyright © 2019 Yannick Heinrich. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdvancedOperationsKit.
+FOUNDATION_EXPORT double AdvancedOperationsKitVersionNumber;
+
+//! Project version string for AdvancedOperationsKit.
+FOUNDATION_EXPORT const unsigned char AdvancedOperationsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdvancedOperationsKit/PublicHeader.h>
+
+
